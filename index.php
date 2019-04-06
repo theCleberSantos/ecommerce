527ff6f4ff923c66f2822bc6147b6564da55a11c
theCleberSantos <cleberson.181@gmail.com>
2019-04-06 03:16:29 -0300

    Criação da class Page

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\DB\Sql();
-	
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
 
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
 $app->run();
 
- ?>
+?>
